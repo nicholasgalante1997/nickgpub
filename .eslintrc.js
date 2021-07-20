374a12cf2953182396900a0bcd9b7aaad40809b6
nicholasgalante1997 <nicholasgalante1997@gmail.com>
2021-07-19 18:17:56 -0600

    test dir

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,7 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
+    'jest/globals': true,
   },
   extends: [
     'plugin:react/recommended',
@@ -16,7 +17,13 @@ module.exports = {
   },
   plugins: [
     'react',
+    'jest',
   ],
   rules: {
+    'jest/no-disabled-tests': 'warn',
+    'jest/no-focused-tests': 'error',
+    'jest/no-identical-title': 'error',
+    'jest/prefer-to-have-length': 'warn',
+    'jest/valid-expect': 'error',
   },
 };
